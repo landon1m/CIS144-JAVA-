dc854c1d57ebec2fc6aad6f35748d58e4b0cbb3e
Landon Blackburn <landon1m2@yahoo.com>
2023-03-03 10:40:52 -0600

    Create a Main Java file
    This should be the starting point of your program

---
+++ Main.java
@@ -0,0 +1,12 @@
+//This is how your main file should look
+//The file must be saved as the className.java
+//in this case it will be Main.java
+public class Main {
+            //notice the () contain a value
+    public static void main(String[] args){
+        //calling the LinkedClass
+        //seperated with a .
+        //then call the linked method with linkedMethod()
+        LinkedClass.linkedMethod();
+    };
+};
